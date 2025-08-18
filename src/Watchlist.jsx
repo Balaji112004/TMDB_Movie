159229f2b2f58798734be646b3e6aeec67d417b4
Balaji112004 <k.balaji112004@gmail.com>
2025-08-18 23:15:03 +0530

    Update Watchlist.jsx

--- src/Watchlist.jsx
+++ src/Watchlist.jsx
@@ -28,17 +28,17 @@ function Watchlist({ watch, toggleWatch, setwatch }) {
   }, [watch]);
 
   return (
-    <div>
+   <div>
       {/* Genre Buttons */}
-      <div className="my-5 flex justify-center">
+      <div className="my-5 flex flex-row flex-wrap justify-center">
         {genrelist.map((genre, index) => (
           <div
             key={index}
             onClick={() => genrehandler(genre)}
             className={
               genre === curgen
-                ? "font-bold text-white bg-blue-400 w-[140px] p-3 rounded-xl flex justify-center mx-3 hover:cursor-pointer"
-                : "font-bold text-white bg-gray-400 w-[140px] p-3 rounded-xl flex justify-center mx-3 hover:cursor-pointer"
+                ? "font-xs lg:font-bold text-white bg-blue-400 m-2 sm:h-[20px] w-[100px] p-1 lg:w-[140px] lg:h-[50px] lg:p-3 rounded-xl flex justify-center mx-3 hover:cursor-pointer"
+                : "font-xs lg:font-bold text-white bg-gray-400 m-2 sm:h-[20px] w-[100px] p-1 lg:w-[140px] lg:h-[50px] lg:p-3 rounded-xl flex justify-center mx-3 hover:cursor-pointer"
             }
           >
             {genre}
@@ -62,20 +62,21 @@ function Watchlist({ watch, toggleWatch, setwatch }) {
         <table className="w-full text-center text-gray-500">
           <thead className="border-b-2">
             <tr>
-              <th className="w-[50%] p-1">Name</th>
-              <th>
-                <div>
+              <th className=" text-xs lg:text-lg lg:w-[30%] p-1">Name</th>
+              <th className="text-xs lg:text-lg">
+                <div className="">
                   <button onClick={sortDecreasing}>
-                    <i className="bx bx-up-arrow-alt pr-3"></i>
+                    <i className="bx bx-up-arrow-alt lg:pr-3"></i>
                   </button>
                   Ratings
+                  
                   <button onClick={sortIncreasing}>
-                    <i className="bx bx-down-arrow-alt pl-3"></i>
+                    <i className="bx bx-down-arrow-alt lg:pl-3"></i>
                   </button>
                 </div>
               </th>
-              <th>Popularity</th>
-              <th>Genre</th>
+              <th className="text-xs lg:text-lg pl-1">Popularity</th>
+              <th className="text-xs lg:text-lg">Genre</th>
               <th></th>
             </tr>
           </thead>
@@ -86,21 +87,21 @@ function Watchlist({ watch, toggleWatch, setwatch }) {
               )
               .filter((lis) => lis.title.toLowerCase().includes(search.toLowerCase()))
               .map((lis, index) => (
-                <tr key={index} className="border-b border-gray-500">
-                  <td>
+                <tr key={index} className="border-b border-gray-500 h-[250px]">
+                  <td className=" w-[250px]">
                     <div className="w-full m-1 relative">
                       <img
                         className="w-[100px]"
                         src={`https://image.tmdb.org/t/p/original/${lis.poster_path}`}
                         alt=""
                       />
-                      <p className="absolute left-35 top-15">{lis.title}</p>
+                      <p className="text-xs lg:text-lg absolute left-2 lg:absolute lg:left-35 top-33 lg:top-15">{lis.title}</p>
                     </div>
                   </td>
-                  <td>{lis.vote_average}</td>
-                  <td>{lis.popularity}</td>
-                  <td>{Genreids[lis.genre_ids[0]]}</td>
-                  <td>
+                  <td className="text-xs lg:text-lg">{lis.vote_average}</td>
+                  <td className="text-xs lg:text-lg">{lis.popularity}</td>
+                  <td className="text-xs lg:text-lg">{Genreids[lis.genre_ids[0]]}</td>
+                  <td className="text-xs lg:text-lg">
                     <button
                       className="text-red-500 font-semibold"
                       onClick={() => toggleWatch(lis)}
