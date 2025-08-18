be9672d01dc5008ebde8416f7989449b283072fd
Balaji112004 <k.balaji112004@gmail.com>
2025-08-18 23:12:08 +0530

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -10,11 +10,12 @@ function Nav() {
             {/* <i className='bx bx-camera-movie text-4xl p-3 mr-1'></i> */}
             
             <div className='m-2 '>
-              <img className='h-[45px] w-[150px]' src={logo} alt="" />
+              <img className='h-[30px] w-[100px] lg:h-[45px] lg:w-[150px]' src={logo} alt="" />
             </div>
-            <div className='mr-8'>
-            <Link to="/movies" className='p-3 text-xl font-semibold text-purple-500 hover:font-bold hover:text-purple-600 hover:cursor-pointer'>Movies</Link>
-            <Link to="/watchlist" className='p-3  text-xl text-bold-700 font-semibold text-purple-500 hover:font-bold hover:text-purple-600 hover:cursor-pointer'>Watchlist</Link>
+            <div className='mr-2 lg:mr-8'>
+            <Link to="/movies" className='text-sm mr-2
+             p-1 lg:p-3 lg:text-xl font-semibold text-purple-500 hover:font-bold hover:text-purple-600 hover:cursor-pointer'>Movies</Link>
+            <Link to="/watchlist" className='text-sm p-1 lg:p-3  lg:text-xl text-bold-700 font-semibold text-purple-500 hover:font-bold hover:text-purple-600 hover:cursor-pointer'>Watchlist</Link>
             </div>
         </div>
         <div>
