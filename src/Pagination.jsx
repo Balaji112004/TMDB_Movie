06031413bfd41e7a574daea5045a372c72329951
Balaji112004 <k.balaji112004@gmail.com>
2025-10-30 14:04:15 +0530

    Update Pagination.jsx

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Pagination({Incre,Reduce,count}) {
   return (
-    <div className="w-full flex justify-center bg-gray-900/60 mt-10">
-      <button className="text-4xl pr-3 hover:translate-x-[-3px] duration:400">
+    <div className="w-full flex justify-center mt-10 mb-6">
+      <button className="text-4xl bg-gray-300 border-2">
         <i className="bx bx-chevron-left" onClick={Reduce}></i>
       </button>
-      <p className="text-2xl mt-[2px]">{count} </p>
-      <button className="text-4xl pl-3 hover:translate-x-[3px] duration:400">
+      <p className="text-2xl px-5 ml-1 mt-[5px]">{count} </p>
+      <button className="text-4xl bg-gray-300 border-2 ">
         <i className="bx bx-chevron-right" onClick={Incre}></i>
       </button>
     </div>
